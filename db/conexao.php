4081fa6eb7c6c81b1ad90a270269940e7a5c6f07
Ricardo Ferreira <ricardo.lazafer@gmail.com>
2021-10-13 21:08:39 -0300

    Commit - Criação do código para criação do BD (Concluído)

--- db/conexao.php
+++ db/conexao.php
@@ -1,23 +1,51 @@
 <?php
 
 function novaConexao($banco = 'curso_php'){
-    // Acesso ao DB
-    $servidor = 'localhost:3306';
+    $servidor = '127.0.0.1';
     $usuario = 'root';
     $senha = 'root';
 
-    //Criando uma nova instancia do MySQLI
     $conexao = new mysqli($servidor, $usuario, $senha, $banco);
 
+    if($conexao->connect_error){
+        die('erro: ' . $conexao->connect_error);
+    }
+
+    return $conexao;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function novaConexao($banco = 'curso_php'){
+    // Acesso ao DB
+  //  $servidor = 'localhost:3306';
+    //$usuario = 'root';
+    //$senha = 'root';
+
+    //Criando uma nova instancia do MySQLI
+    //$conexao = new mysqli($servidor, $usuario, $senha, $banco);
+
     // Verificando se o BD foi conectado
-    if($conexao -> connect_error){
+    //if($conexao -> connect_error){
         // Duas possibilidades para tratamento de erro
-        die('erro: ' . $conexao -> connect_error); // Não usar em projetos reais
+      //  die('erro: ' . $conexao -> connect_error); // Não usar em projetos reais
         /** 
          * echo('erro: ' . $conexao -> connect_error);
          * exit();
         */
-        return $conexao;
+        //return $conexao;
         
-    }
-}
+    //}
+//}
--- index.php
+++ index.php
@@ -15,76 +15,9 @@
     </header>
     <main class="principal">
         <div class="conteudo">
-            <nav class="modulos">
-
-            <div class="modulo laranja">
-                <h3>15. BD</h3>
-                <ul>
-                    <li><a href="exercicio.php?dir=db&file=criar_banco">Criar Banco</a></li>
-                </ul>
-            </div>
-
-            <div class="modulo roxo">
-                <h3>14. Formulário</h3>
-                <ul>
-                    <li><a href="exercicio.php?dir=formulario&file=formulario">Formulário</a></li>
-                </ul>
-            </div>
-
-            <div class="modulo roxo">
-                <h3>4. Controle</h3>
-                <ul>
-                    <li><a href="exercicio.php?dir=controle&file=if_else">If Else</a></li>
-                    <li><a href="exercicio.php?dir=controle&file=operadores_relacionais">Operadores Relacionais</a></li>
-                    <li><a href="exercicio.php?dir=controle&file=desafio_pi">Desafio PI</a></li>
-                    <li><a href="exercicio.php?dir=controle&file=operadores_logicos">Operadores Lógicos</a></li>
-                    <li><a href="exercicio.php?dir=controle&file=desafio_operadores_logicos">Desafio Operadores Lógicos</a></li>
-                    <li><a href="exercicio.php?dir=controle&file=operador_ternario">Operador Ternário</a></li>
-                    <li><a href="exercicio.php?dir=controle&file=testes_if_else">Projeto Luz (Teste)</a></li>
-                    <li><a href="exercicio.php?dir=controle&file=tabela_futebol">Projeto Tabela Futebol (Teste)</a></li>
-                    <li><a href="exercicio.php?dir=controle&file=switch">Switch</a></li>
-                </ul>
-            </div>
-
-            <div class="modulo azul">
-                <h3>3. Variáveis</h3>
-                    <ul>
-                        <li><a href="exercicio.php?dir=variaveis&file=basico">Variáveis</a></li>
-                        <li><a href="exercicio.php?dir=variaveis&file=desafio_equacao">Desafio Equação</a></li>
-                        <li><a href="exercicio.php?dir=variaveis&file=atribuicoes">Atribuições</a></li>
-                        <li><a href="exercicio.php?dir=variaveis&file=interpolacao">Interpolação</a></li>
-                        <li><a href="exercicio.php?dir=variaveis&file=variaveis_variaveis">Variáveis Variáveis</a></li>
-                        <li><a href="exercicio.php?dir=variaveis&file=desafio_variaveis">Desafio Variáveis</a></li>
-                        <li><a href="exercicio.php?dir=variaveis&file=valor_referencia">Valor vs Referencia</a></li>
-                        <li><a href="exercicio.php?dir=variaveis&file=constantes">Constantes</a></li>
-                    </ul>
-            </div>
-
-            <div class="modulo vermelho">
-                <h3>2. Tipos</h3>
-                    <ul>
-                        <li><a href="exercicio.php?dir=tipos&file=int">Tipo Inteiro</a></li>
-                        <li><a href="exercicio.php?dir=tipos&file=float">Tipo Float</a></li>
-                        <li><a href="exercicio.php?dir=tipos&file=aritmeticos">Operações Aritméticas</a></li>
-                        <li><a href="exercicio.php?dir=tipos&file=desafio_precedencia">Desafio Precedêndia</a></li>
-                        <li><a href="exercicio.php?dir=tipos&file=string">Tipo String</a></li>
-                        <li><a href="exercicio.php?dir=tipos&file=desafio_string">Desafio String</a></li>
-                        <li><a href="exercicio.php?dir=tipos&file=booleano">Tipo Booleano</a></li>
-                        <li><a href="exercicio.php?dir=tipos&file=conversoes">Conversões</a></li>
-                    </ul>
-            </div>
-
-            <div class="modulo verde">
-                <h3>1. Básico</h3>
-                    <ul>
-                        <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
-                        <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
-                        <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
-                        <li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
-                        <li><a href="exercicio.php?dir=basico&file=desafio">Desafio</a></li>
-                    </ul>
-            </div>
-            </nav>
+            <?php
+                require_once('menu.php');
+            ?>
         </div>
     </main>
     <footer class="rodape">
